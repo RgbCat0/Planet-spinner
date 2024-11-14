98a0abf0b72f6c6b839800f77565b667e57d23e1
Jesper Heese <77155517+RgbCat0@users.noreply.github.com>
2024-11-14 16:06:09 +0100

    added scenes and other stuff

---
+++ Assets/multiplayerTest.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Unity.Multiplayer.Widgets;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+
+
+public class multiplayerTest : MonoBehaviour
+{
+    private async void Start()
+    {
+        await SetupAsync();
+    }
+    internal static async Task SetupAsync()
+    {
+                
+        if (UnityServices.State != ServicesInitializationState.Initialized)
+        {
+            await UnityServices.InitializeAsync();
+            Debug.Log("Initialized Unity Services");
+        }
+
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            await AuthenticationService.Instance.UpdatePlayerNameAsync("rgb");
+            var name = await AuthenticationService.Instance.GetPlayerNameAsync(false);
+            Debug.Log($"Signed in anonymously. Name: {name}. ID: {AuthenticationService.Instance.PlayerId}");
+        }
+        
+
+        ServicesInitialized();
+    }
+
+    /// <summary>
+    /// Called after all services are initialized.
+    /// </summary>
+    public static void ServicesInitialized()
+    {
+        WidgetServiceInitialization.ServicesInitialized();
+    }
+}
